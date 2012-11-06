7f6e5a0eb8f37442e93e06cbe7577f13b7101f5a
wolfhesse <wolfgang.schuessel@gmail.com>
2012-11-06 14:01:00 +0100

    wp

---
+++ src/export-cats.js
@@ -0,0 +1,36 @@
+var fs=require('fs');
+var mongoose = require('mongoose');
+var db = mongoose.createConnection('localhost', 'test');
+
+var schema = mongoose.Schema({ name: 'string' });
+var Cat = db.model('Cat', schema);
+
+nl = function (s) {
+    s.write("\n");
+}
+Cat.find(function (err, kittens) {
+    if (err) {
+        console.log(err)
+    }
+    else {
+        // write kittens to file
+
+        var s = fs.createWriteStream('kittens.csv',{flags:'w'});
+        s.write('name');
+        nl(s);
+        kittens.forEach(function(k){
+            s.write(k.name);
+            nl(s);
+        });
+        s.end();
+        s = fs.createWriteStream('kittens.json',{flags:'w'});
+        s.end(JSON.stringify({'kittens':kittens}));
+        console.log('wrote kittens files');
+        db.close();
+    }
+});
+
+console.log('through');
+
+db.close();
+
